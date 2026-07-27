dd95381203a3a1d1b56f711f8e31d321d8452ce9
falafilm <falafilm03@gmail.com>
2026-07-27 16:55:21 +0700

    update compose to mongo

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return("Hi Baby")
-    
+    return("<h1>Welcome to the Home Page</h1>")
+
+@app.route('/about')
+
+def about():
+    return("<h1>About Mr: Nong'Sudarat 2026</h1>")
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000) 
